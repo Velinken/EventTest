7b2911a049c6443697d093f0ba9ddfe83c184c5c
Dzendo <ASDzendo@gmail.com>
2021-06-26 16:20:00 +0300

    ViewModel start 1

---
+++ app/src/test/java/com/dinadurykina/eventtest/ui/fragment/FragmentViewModelTest.kt
@@ -0,0 +1,41 @@
+package com.dinadurykina.eventtest.ui.fragment
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class FragmentViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun onToast() {
+    }
+
+    @Test
+    fun keybooardON() {
+    }
+
+    @Test
+    fun keybooardOFF() {
+    }
+
+    @Test
+    fun onSnackbar() {
+    }
+
+    @Test
+    fun onNotify() {
+    }
+}
